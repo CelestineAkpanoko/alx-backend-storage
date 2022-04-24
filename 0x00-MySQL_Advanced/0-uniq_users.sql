cfaeb98fcee893948bd5fc18a90fbd14b1ac05b0
Celestine Akpanoko <celestineakpanoko@gmail.com>
2022-04-24 23:50:55 +0100

    creates users table

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- An SQL script that creates a table users
+
+CREATE TABLE if NOT EXISTS `users` (
+	`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	`email` VARCHAR(255) NOT NULL UNIQUE,
+	`name` VARCHAR(255)
+);
